96986e0575164e17553159c81ceacf250d749a83
BoazGur <64284870+BoazGur@users.noreply.github.com>
2022-02-20 18:42:35 +0200

    Update PALTIER.ino

--- PALTIER.ino
+++ PALTIER.ino
@@ -12,7 +12,7 @@ void loop() {
   float tempature0 = (sensor0 * 0.48828125); // the T equation 500/1024
   Serial.print("tempature0 is: "); 
   Serial.println(tempature0); // print the number on the serial monitor
-ז  
+  
   int sensor1 = analogRead(A1); // Read the data from the sensor (range 0 to 1023).
   float tempature1 = (sensor1 * 0.48828125); // the T equation 500/1024
   Serial.print("tempature1 is: "); 
