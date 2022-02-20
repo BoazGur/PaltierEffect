1ff399330dd697f8595d8dd4d7544d08f14bd8de
BoazGur <64284870+BoazGur@users.noreply.github.com>
2022-02-20 20:57:37 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,6 +10,7 @@ def main():
     for idx, path in enumerate(paths):
         temp0 = []
         temp1 = []
+        delta = []
 
         with open(path, 'r') as file:
             lines = file.readlines()
@@ -20,21 +21,25 @@ def main():
                     if '1AMPER.txt' in path:
                         temp0.append(float(splited[4]))
                         temp1.append(float(splited[1]))
+                        delta.append(float(splited[4]) - float(splited[1]))
                     else:                       
                         temp0.append(float(splited[1]))
                         temp1.append(float(splited[4]))
+                        delta.append(float(splited[1]) - float(splited[4]))
 
         time = np.arange(0, max(len(temp1), len(temp0)) * 0.5, 0.5) # Sampled every 500ms
 
-        data = {'temp0': temp0, 'temp1': temp1, 'time': time}
+        data = {'temp0': temp0, 'temp1': temp1, 'time': time, 'delta': delta}
         df = pd.DataFrame(data)
 
         sns.set_theme(style="darkgrid")
         sns.scatterplot(x='time', y='temp0', data=df)
         sns.scatterplot(x='time', y='temp1', data=df)
-        plt.errorbar(df.time, df.temp0, yerr=0.1 * df.temp0, alpha=0.2)
-        plt.errorbar(df.time, df.temp1, yerr=0.1 * df.temp1, alpha=0.2)
-        plt.legend(labels=["temp0", "temp1"])
+        sns.scatterplot(x='time', y='delta', data=df)
+        plt.errorbar(df.time, df.temp0, yerr=0.5, alpha=0.2)
+        plt.errorbar(df.time, df.temp1, yerr=0.5, alpha=0.2)
+        plt.errorbar(df.time, df.delta, yerr=1, alpha=0.2)
+        plt.legend(labels=["temp0", "temp1", 'delta'])
         plt.xlabel('time [s]')
         plt.ylabel('temprature [C]')
         plt.title(f'{(idx + 1) * 0.5} Amper')
